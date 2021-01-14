c3cf7286682bce8f64bc2dcbb163fca0ddc2ec64
Cedric Manamela <manamelacedric@gmail.com>
2021-01-14 23:50:45 +0200

    test

--- sw.js
+++ sw.js
@@ -14,7 +14,8 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
       return cache.addAll(assets);
-    })
+    }).then((data) => console.log('data', data))
+    .catch(err => console.log('err', err))
   );
 });
 
