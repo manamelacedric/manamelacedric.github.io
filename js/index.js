4dc2947aaa572143cbf765647a5195c0ba461cbb
Cedric Manamela <manamelacedric@gmail.com>
2021-01-25 22:01:17 +0200

    worked on the menu

--- js/index.js
+++ js/index.js
@@ -1,32 +1,43 @@
 // define vars
+const menuBtn = document.querySelector('.menu-btn')
+const nav = document.querySelector('nav')
+let menuOpen = false
 
+/*
+const header = document.querySelector('.main-header');
+
+window.addEventListener('scroll', () => {
+  const scrollPos = window.scrollY;
+  if(scrollPos > 10){
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+})
+*/
 
 // Register Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log(err));
-  });
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function () {
+		navigator.serviceWorker
+			.register('/sw.js')
+			.then((res) => console.log('service worker registered'))
+			.catch((err) => console.log(err))
+	})
 }
 
-const menuBtn = document.querySelector(".menu-btn");
-let menuOpen = false;
-menuBtn.addEventListener("click", () => {
-  console.log({ menuOpen });
-  if (!menuOpen) {
-    menuBtn.classList.add("open");
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove("open");
-    menuOpen = false;
-  }
-});
-
+menuBtn.addEventListener('click', () => {
+	if (!menuOpen) {
+    menuBtn.classList.add('menu-open')
+    nav.classList.add('nav-open')
+		menuOpen = true
+	} else {
+    menuBtn.classList.remove('menu-open')
+    nav.classList.remove('nav-open')
+		menuOpen = false
+	}
+})
 
 const observer = new IntersectionObserver(test, {})
 
-function test() {
-  
-}
+function test() {}
